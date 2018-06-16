128720d54a6620f8fee30d82d5f5a22f4fe50942
Resslen <robbertk2@gmail.com>
2018-06-16 18:47:59 +0200

    context changes

--- ImageCloudMVC/App_Start/Startup.Auth.cs
+++ ImageCloudMVC/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using ImageCloudMVC.Models;
+using ImageCloudMVC.DAL;
 
 namespace ImageCloudMVC
 {
@@ -15,7 +16,7 @@ namespace ImageCloudMVC
         public void ConfigureAuth(IAppBuilder app)
         {
             // Configure the db context, user manager and signin manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext(ImageCloudContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
--- ImageCloudMVC/DAL/ImageCloudContext.cs
+++ ImageCloudMVC/DAL/ImageCloudContext.cs
@@ -1,13 +1,36 @@
 ﻿using System.Data.Entity;
+using System.Security.Claims;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Threading.Tasks;
 
 namespace ImageCloudMVC.DAL
 {
-    public class ImageCloudContext : DbContext
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ImageCloudContext : IdentityDbContext<ApplicationUser>
     {
         public ImageCloudContext() : base("ImageCloudConnString")
         {}
 
+        public static ImageCloudContext Create()
+        {
+            return new ImageCloudContext();
+        }
+
         public DbSet<File> Files { get; set; }
         public DbSet<Folder> Folders { get; set; }
     }
+
 }
